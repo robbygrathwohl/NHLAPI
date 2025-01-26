83397e11957228f8eb4065989b30f6be9fa037df
Robert Grathwohl <robbygrathwohl@gmail.com>
2025-01-26 03:21:51 -0600

    second

--- nhl_api/tests/player.py
+++ nhl_api/tests/player.py
@@ -3,5 +3,5 @@ from nhl_api.base_models import Player
 
 nhlapi = RestAdapter(hostname='api-web.nhle.com')
 result = nhlapi.get('player/8478402/landing')
-player = Player(**result.data)
+player = Player(**result.data[0])
 print(player.player_id, player.first_name, player.last_name)
